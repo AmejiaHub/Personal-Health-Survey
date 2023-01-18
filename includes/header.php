ff9bf29d97253dafd6d49c9e98b57d0eaed28180
Albert <a.mejia@gmx.net>
2023-01-18 11:59:44 +0100

    update commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php 
+echo
+    '<header class="d-flex flex-wrap justifiy-content-center py-3 mb-4 border-bottom">
+        <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" href="/"></a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link-active" href="#" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Previous Page</a>
+                </li>
+            </ul>
+    </header>';
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Health Survey</title>
+    <!-- Bootstrap5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Internal CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+    <!-- Include PHP Header -->
+    <div class="container" id="header">
+        <?php include 'header.php';?>
+    </div>
+
+    <!-- Start Body of Page -->
+
+
+
+    <!-- End Body of Page -->
+
+    <!-- Include PHP Footer -->
+    <div class="container" id="footer">
+        <?php include 'footer.php';?>
+    </div>
+
+<body>
+    <!-- Bootstrap5 Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
