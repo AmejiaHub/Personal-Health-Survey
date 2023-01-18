997da6518f218331ac99a53276aec45c7189f7ac
Albert <a.mejia@gmx.net>
2023-01-18 16:12:13 +0100

    update commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,8 @@
 <?php 
 echo
-    '<footer class="text-black text-center">
+ '<footer class="text-black text-center no-padding-margin">
     <div class="text-center p-3">
-      <h5 class="text-uppercase">Health Survey:</h5>
-      <br>
+      <h5 class="text-uppercase">Health Survey:</h5><br>
 
       <p>
         This health survey helps you finding out some information about your current health.<br><br>
@@ -12,7 +11,7 @@ echo
     </div>
 
     <!-- Copyright -->
-    <div class="text-center" style="background-color: rgba(0, 0, 0, 0.2)">
+    <div id="copyright" class="text-center" style="background-color: rgba(0, 0, 0, 0.2)">
       © 2023 Copyright:
       <p>Albert Mejia</p>
     </div>
--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,28 @@
 <?php 
 echo
-    ' <header>
+  '<header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <!-- Container wrapper -->
+    <div class="container">
+      <!-- Navbar logo -->
+      <div class="logo"><img src="/images/logo-removebg-preview.png" alt="logo"/></div>
+    </div>
+    <div class="container">
+      <ul class="nav nav-pills d-flex justify-content-center justify-content-lg-end">
+          <li class="nav-item"><a class="home_button" href="#" aria-current="page">Home</a></li>
+          <li class="nav-item"><a class="aboutus_button" href="#" aria-current="page">About Us</a></li>
+      </ul>
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+</header>'
+
+
+
+
+    /*' <header>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
       <!-- Container wrapper -->
@@ -17,5 +39,5 @@ echo
       <!-- Container wrapper -->
     </nav>
     <!-- Navbar -->
-  </header>'
+  </header>'*/
 ?>
