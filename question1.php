4cb3fea634702c79769901efb6d1ba9e61488ddc
Albert <a.mejia@gmx.net>
2023-01-19 16:18:11 +0100

    update commit

--- question1.php
+++ question1.php
@@ -20,8 +20,21 @@
     <!-- Start Content of Page -->
 
     <div class="container-fluid">
-        <div class="question_container>
-        <p>How healthy are you physically?</p>
+        <div id="question_box">
+            <p>Question 1/10</p>
+            <p>How healthy are you?</p>
+            <div class="range_slider">
+                <div class="range-text">
+                    <div class="rating">1</div>
+                    <div class="rating">2</div>
+                    <div class="rating">3</div>
+                    <div class="rating">4</div>
+                    <div class="rating">5</div>
+                </div>
+            <input type="range" class="form-range" min="1" max="5" step="1" id="customRange3">
+            </div>
+            <br>
+            <button type="button" onclick="question2.php">Next Question</button>
         </div>
     </div>
 
